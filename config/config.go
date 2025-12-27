09236a254f4305fba405bf2b5d6674288c19bc02
Dhanavadh T. Saito <105502355+dhanavadh@users.noreply.github.com>
2025-12-27 10:27:57 +0700

    add config

---
+++ config/config.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	ServerPort        string
+	GinMode           string
+	DBHost            string
+	DBPort            string
+	DBUser            string
+	DBPassword        string
+	DBName            string
+	DBSSLMode         string
+	R2AccountID       string
+	R2AccessKey       string
+	R2SecretAccessKey string
+	R2BucketName      string
+}
+
+func Load() *Config {
+	godotenv.Load()
+	return &Config{
+		ServerPort:        os.Getenv("SERVER_PORT"),
+		GinMode:           os.Getenv("GIN_MODE"),
+		DBHost:            os.Getenv("DB_HOST"),
+		DBPort:            os.Getenv("DB_PORT"),
+		DBUser:            os.Getenv("DB_USER"),
+		DBPassword:        os.Getenv("DB_PASSWORD"),
+		DBName:            os.Getenv("DB_NAME"),
+		DBSSLMode:         os.Getenv("DB_SSL_MODE"),
+		R2AccountID:       os.Getenv("R2_ACCOUNT_ID"),
+		R2AccessKey:       os.Getenv("R2_ACCESS_KEY"),
+		R2SecretAccessKey: os.Getenv("R2_SECRET_ACCESS_KEY"),
+		R2BucketName:      os.Getenv("R2_BUCKET_NAME"),
+	}
+}
+
+func (c *Config) GetDSN() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", c.DBHost, c.DBPort, c.DBUser, c.DBPassword, c.DBName, c.DBSSLMode)
+}
